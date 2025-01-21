fa7c5f9d2977c375977098357e06566ef9c5047c
Nico <nicolo.rizzi@outlook..it>
2025-01-21 21:25:09 +0100

    fixed some stuff

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import NoPastiForm from '../components/NoPastiForm';
+import NoPastiForm from '../components/NoPastiForm.js'
 
 export default function Home() {
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-2xl font-bold text-center mb-8">Form No Pasti</h1>
+    <div>
+      <h1>Form No Pasti</h1>
       <NoPastiForm />
     </div>
   );
